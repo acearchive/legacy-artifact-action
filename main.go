22e528d3ba6c418a2fd78395873d3348dad62146
Frawley <frawleyskid@gmail.com>
2022-01-05 17:37:24 -0500

    Add code for parsing artifact files

--- main.go
+++ main.go
@@ -1,19 +1,3 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
-func main() {
-	workspaceDir := os.Getenv("GITHUB_WORKSPACE")
-
-	entries, err := os.ReadDir(workspaceDir)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, entry := range entries {
-		fmt.Println(entry.Name())
-	}
-}
+func main() {}
---
+++ parse/parse.go
@@ -0,0 +1,134 @@
+package parse
+
+import (
+	"bufio"
+	"errors"
+	"github.com/ipfs/go-cid"
+	"gopkg.in/yaml.v2"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const frontMatterDelimiter = "---"
+
+var ErrNoFrontMatter = errors.New("this file has no front matter")
+
+func findArtifactFiles(workspacePath, pathGlob string) ([]string, error) {
+	return filepath.Glob(filepath.Join(workspacePath, pathGlob))
+}
+
+func isFrontMatterDelimiter(line string) bool {
+	return strings.HasPrefix(line, frontMatterDelimiter) && strings.TrimSpace(line) == frontMatterDelimiter
+}
+
+func isWhitespace(line string) bool {
+	return strings.TrimSpace(line) == ""
+}
+
+func extractFrontMatter(path string) (string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+
+	defer func() {
+		closeErr := file.Close()
+		if closeErr != nil && err == nil {
+			err = closeErr
+		}
+	}()
+
+	scanner := bufio.NewScanner(file)
+
+	// Find the start of the front matter block.
+findStart:
+	for {
+		scanner.Scan()
+		if scanner.Err() != nil {
+			return "", err
+		}
+
+		currentLine := scanner.Text()
+
+		switch {
+		case isWhitespace(currentLine):
+			continue
+		case isFrontMatterDelimiter(currentLine):
+			break findStart
+		default:
+			return "", ErrNoFrontMatter
+		}
+	}
+
+	var frontMatter strings.Builder
+
+	for {
+		scanner.Scan()
+		if scanner.Err() != nil {
+			return "", err
+		}
+
+		currentLine := scanner.Text()
+
+		if isFrontMatterDelimiter(currentLine) {
+			break
+		}
+
+		frontMatter.WriteString(currentLine + "\n")
+	}
+
+	return frontMatter.String(), nil
+}
+
+type artifactFileEntry struct {
+	Cid string `yaml:"cid"`
+}
+
+type artifactEntry struct {
+	Files []artifactFileEntry `yaml:"files"`
+}
+
+func extractArtifactCids(frontMatter string) ([]cid.Cid, error) {
+	artifact := artifactEntry{}
+	if err := yaml.Unmarshal([]byte(frontMatter), &artifact); err != nil {
+		return nil, err
+	}
+
+	cidList := make([]cid.Cid, len(artifact.Files))
+
+	for i, artifactFile := range artifact.Files {
+		artifactCid, err := cid.Parse(artifactFile.Cid)
+		if err != nil {
+			return nil, err
+		}
+
+		cidList[i] = artifactCid
+	}
+
+	return cidList, nil
+}
+
+func ParseArtifactFiles(workspacePath, pathGlob string) ([]cid.Cid, error) {
+	artifactFilePaths, err := findArtifactFiles(workspacePath, pathGlob)
+	if err != nil {
+		return nil, err
+	}
+
+	cidList := make([]cid.Cid, 0, len(artifactFilePaths))
+	for _, filePath := range artifactFilePaths {
+		frontMatter, err := extractFrontMatter(filePath)
+		if err != nil {
+			return nil, err
+		}
+
+		currentCidList, err := extractArtifactCids(frontMatter)
+		if err != nil {
+			return nil, err
+		}
+
+		cidList = append(cidList, currentCidList...)
+	}
+
+	return cidList, nil
+}
