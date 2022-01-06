a3debf82f4b8d1f07cec7718eeaaffd667d580e5
Frawley <frawleyskid@gmail.com>
2022-01-06 09:00:59 -0500

    Add default API server address

--- upload/client.go
+++ upload/client.go
@@ -2,10 +2,13 @@ package upload
 
 import (
 	api "github.com/ipfs/go-ipfs-http-client"
+	"github.com/multiformats/go-multiaddr"
 )
 
+const defaultClientApiAddr = "/ip4/127.0.0.1/tcp/5001"
+
 func NewClient() (*api.HttpApi, error) {
-	addr, err := api.ApiAddr(api.DefaultPathRoot)
+	addr, err := multiaddr.NewMultiaddr(defaultClientApiAddr)
 	if err != nil {
 		return nil, err
 	}
