f1882f5a39771ef348c697d832a17df6cc592f36
Frawley <frawleyskid@gmail.com>
2022-05-10 21:17:52 -0400

    Remove duplicate structs

--- output/json.go
+++ output/json.go
@@ -13,86 +13,11 @@ const prettyJsonIndent = "  "
 
 var ErrInvalidOutput = errors.New("invalid output parameter")
 
-type ArtifactFileOutput struct {
-	Name      string  `json:"name"`
-	MediaType *string `json:"mediaType"`
-	Filename  *string `json:"filename"`
-	Cid       string  `json:"cid"`
-}
-
-type ArtifactLinkOutput struct {
-	Name string `json:"name"`
-	Url  string `json:"url"`
-}
-
-type ArtifactEntryOutput struct {
-	Version         int                  `json:"version"`
-	Title           string               `json:"title"`
-	Description     string               `json:"description"`
-	LongDescription *string              `json:"longDescription"`
-	Files           []ArtifactFileOutput `json:"files"`
-	Links           []ArtifactLinkOutput `json:"links"`
-	People          []string             `json:"people"`
-	Identities      []string             `json:"identities"`
-	FromYear        int                  `json:"fromYear"`
-	ToYear          *int                 `json:"toYear"`
-	Decades         []int                `json:"decades"`
-	Aliases         []string             `json:"aliases"`
-}
-
-type ArtifactOutput struct {
-	Path  string              `json:"path"`
-	Slug  string              `json:"slug"`
-	Rev   *string             `json:"rev"`
-	Entry ArtifactEntryOutput `json:"entry"`
-}
-
 type Output struct {
-	Artifacts []ArtifactOutput `json:"artifacts"`
+	Artifacts []parse.Artifact `json:"artifacts"`
 }
 
-func marshalArtifact(entries []parse.Artifact, pretty bool) (string, error) {
-	artifacts := make([]ArtifactOutput, len(entries))
-	for entryIndex, artifact := range entries {
-		files := make([]ArtifactFileOutput, len(artifact.Entry.Files))
-		for fileIndex, fileEntry := range artifact.Entry.Files {
-			files[fileIndex] = ArtifactFileOutput{
-				Name:      fileEntry.Name,
-				MediaType: fileEntry.MediaType,
-				Filename:  fileEntry.Filename,
-				Cid:       fileEntry.Cid,
-			}
-		}
-
-		links := make([]ArtifactLinkOutput, len(artifact.Entry.Links))
-		for linkIndex, linkEntry := range artifact.Entry.Links {
-			links[linkIndex] = ArtifactLinkOutput{
-				Name: linkEntry.Name,
-				Url:  linkEntry.Url,
-			}
-		}
-
-		artifacts[entryIndex] = ArtifactOutput{
-			Path: artifact.Path,
-			Slug: artifact.Slug,
-			Rev:  artifact.Rev,
-			Entry: ArtifactEntryOutput{
-				Version:         artifact.Entry.Version,
-				Title:           artifact.Entry.Title,
-				Description:     artifact.Entry.Description,
-				LongDescription: artifact.Entry.LongDescription,
-				Files:           files,
-				Links:           links,
-				People:          artifact.Entry.People,
-				Identities:      artifact.Entry.Identities,
-				FromYear:        artifact.Entry.FromYear,
-				ToYear:          artifact.Entry.ToYear,
-				Decades:         artifact.Entry.Decades,
-				Aliases:         artifact.Entry.Aliases,
-			},
-		}
-	}
-
+func marshalArtifact(artifacts []parse.Artifact, pretty bool) (string, error) {
 	output := Output{Artifacts: artifacts}
 
 	var (
--- parse/model.go
+++ parse/model.go
@@ -3,35 +3,35 @@ package parse
 const CurrentArtifactVersion = 1
 
 type Artifact struct {
-	Path  string
-	Slug  string
-	Rev   *string
-	Entry ArtifactEntry
+	Path  string        `json:"path"`
+	Slug  string        `json:"slug"`
+	Rev   *string       `json:"rev"`
+	Entry ArtifactEntry `json:"entry"`
 }
 
 type ArtifactEntryFile struct {
-	Name      string  `yaml:"name"`
-	MediaType *string `yaml:"mediaType"`
-	Filename  *string `yaml:"filename"`
-	Cid       string  `yaml:"cid"`
+	Name      string  `yaml:"name" json:"name"`
+	MediaType *string `yaml:"mediaType" json:"mediaType"`
+	Filename  *string `yaml:"filename" json:"filename"`
+	Cid       string  `yaml:"cid" json:"cid"`
 }
 
 type ArtifactEntryLink struct {
-	Name string `yaml:"name"`
-	Url  string `yaml:"url"`
+	Name string `yaml:"name" json:"name"`
+	Url  string `yaml:"url" json:"url"`
 }
 
 type ArtifactEntry struct {
-	Version         int                 `yaml:"version"`
-	Title           string              `yaml:"title"`
-	Description     string              `yaml:"description"`
-	LongDescription *string             `yaml:"longDescription"`
-	Files           []ArtifactEntryFile `yaml:"files"`
-	Links           []ArtifactEntryLink `yaml:"links"`
-	People          []string            `yaml:"people"`
-	Identities      []string            `yaml:"identities"`
-	FromYear        int                 `yaml:"fromYear"`
-	ToYear          *int                `yaml:"toYear"`
-	Decades         []int               `yaml:"decades"`
-	Aliases         []string            `yaml:"aliases"`
+	Version         int                 `yaml:"version" json:"version"`
+	Title           string              `yaml:"title" json:"title"`
+	Description     string              `yaml:"description" json:"description"`
+	LongDescription *string             `yaml:"longDescription" json:"longDescription"`
+	Files           []ArtifactEntryFile `yaml:"files" json:"files"`
+	Links           []ArtifactEntryLink `yaml:"links" json:"links"`
+	People          []string            `yaml:"people" json:"people"`
+	Identities      []string            `yaml:"identities" json:"identities"`
+	FromYear        int                 `yaml:"fromYear" json:"fromYear"`
+	ToYear          *int                `yaml:"toYear" json:"toYear"`
+	Decades         []int               `yaml:"decades" json:"decades"`
+	Aliases         []string            `yaml:"aliases" json:"aliases"`
 }
